18f1a0ddfe60d98db69c3073140d176c2de28325
Bozhidar Vangelov <bozhidar.vangelov@gmail.com>
2024-10-03 23:35:50 +0300

    feat(project-setup): Environment Configuration #1

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,19 @@
+import theme from "@/theme";
 import { Text, View } from "react-native";
+import { PaperProvider } from "react-native-paper";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>AutoChronos</Text>
-    </View>
+    <PaperProvider theme={theme}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>AutoChronos</Text>
+      </View>
+    </PaperProvider>
   );
 }
---
+++ theme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from "react-native-paper";
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "#4CAF50",
+    accent: "#FF5722",
+  },
+};
+
+export default theme;
