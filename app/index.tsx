eef7390a8ac23f44c0394f869c236cab32e5ea9b
Bozhidar Vangelov <bozhidar.vangelov@gmail.com>
2024-10-06 00:12:31 +0300

    feat(authentication): Login screen #3

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,16 @@
-import theme from "@/theme";
-import { Link } from "expo-router";
 import { View } from "react-native";
-import { PaperProvider } from "react-native-paper";
+import { Text } from "react-native-paper";
 
 export default function Index() {
   return (
-    <PaperProvider theme={theme}>
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Link href="/auth/login">Login</Link>
-        <Link href="/auth/signup">Signup</Link>
-      </View>
-    </PaperProvider>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Home Screen</Text>
+    </View>
   );
 }
--- services/api.ts
+++ services/api.ts
@@ -1,13 +1,14 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 
 const BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL;
 
 const api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: `${BASE_URL}/api`,
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+api.interceptors.request.use(async (config) => {
+  const token = await AsyncStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
